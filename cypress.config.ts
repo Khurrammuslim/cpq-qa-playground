1c4481e5c1ad3e70df5b0228c2caabf0cc6f11ba
Khurram Muslim <Khurram.muslim@gmail.com>
2026-09-23 00:28:53 +0200

    Add CI artifacts and performance testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   allowCypressEnv: false,
 
+  // video: true,
+  // videoCompression: true,
+
   e2e: {
+    specPattern: "cypress/e2e/**/*.cy.ts",
+    supportFile: "cypress/support/e2e.ts",
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
